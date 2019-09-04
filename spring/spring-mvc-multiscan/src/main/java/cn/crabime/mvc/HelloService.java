8c3fdd2d84fd2e17ce9dc3bcb40965b7ea9f9386
crabime <songsx@gooalgene.com>
2019-09-04 17:03:08 +0800

    模拟创建双web application context事件

---
+++ spring/spring-mvc-multiscan/src/main/java/cn/crabime/mvc/HelloService.java
@@ -0,0 +1,31 @@
+package cn.crabime.mvc;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService implements InitializingBean, ApplicationContextAware, BeanFactoryAware {
+    private ApplicationContext applicationContext;
+
+    private BeanFactory beanFactory;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("当前上下文：" + applicationContext.getId());
+    }
+}
---
+++ spring/spring-mvc-multiscan/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>SpringMVC双扫描</title>
+</head>
+<body>
+<h1>SpringMVC双扫描</h1>
+</body>
+</html>
